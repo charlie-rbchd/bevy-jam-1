49787c21e752db26175f86af687b1b0d0dc2315d
Joel Robichaud <robichaud.jo@gmail.com>
2022-02-23 18:03:58 -0500

    Add bevy ldtk integration.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup)
+        .insert_resource(LevelSelection::Index(0))
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("default.ldtk"),
+        ..Default::default()
+    });
 }
