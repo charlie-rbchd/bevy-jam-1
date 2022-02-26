a7989cf4ecdaa3dc3e0175ccd13de3eb5ba83937
Joel Robichaud <robichaud.jo@gmail.com>
2022-02-26 16:12:00 -0500

    Fix camera.

--- src/systems.rs
+++ src/systems.rs
@@ -1,3 +1,4 @@
+use crate::components::*;
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
@@ -10,4 +11,65 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ldtk_handle: asset_server.load("default.ldtk"),
         ..Default::default()
     });
+}
+
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
+
+pub fn camera_fit_inside_current_level(
+    mut camera_query: Query<
+        (&mut bevy::render::camera::OrthographicProjection, &mut Transform),
+        Without<Player>,
+    >,
+    player_query: Query<&Transform, With<Player>>,
+    level_query: Query<
+        (&Transform, &Handle<LdtkLevel>),
+        (Without<OrthographicProjection>, Without<Player>),
+    >,
+    level_selection: Res<LevelSelection>,
+    ldtk_levels: Res<Assets<LdtkLevel>>,
+) {
+    if let Ok(Transform {
+        translation: player_translation,
+        ..
+    }) = player_query.get_single()
+    {
+        let player_translation = player_translation.clone();
+
+        let (mut orthographic_projection, mut camera_transform) = camera_query.single_mut();
+
+        for (level_transform, level_handle) in level_query.iter() {
+            if let Some(ldtk_level) = ldtk_levels.get(level_handle) {
+                let level = &ldtk_level.level;
+                if level_selection.is_match(&0, &level) {
+                    let level_ratio = level.px_wid as f32 / ldtk_level.level.px_hei as f32;
+
+                    orthographic_projection.scaling_mode = bevy::render::camera::ScalingMode::None;
+                    orthographic_projection.bottom = 0.0;
+                    orthographic_projection.left = 0.0;
+                    if level_ratio > ASPECT_RATIO {
+                        // level is wider than the screen
+                        orthographic_projection.top = (level.px_hei as f32 / 9.0).round() * 9.0;
+                        orthographic_projection.right = orthographic_projection.top * ASPECT_RATIO;
+                        camera_transform.translation.x = (player_translation.x
+                            - level_transform.translation.x
+                            - orthographic_projection.right / 2.0)
+                            .clamp(0.0, level.px_wid as f32 - orthographic_projection.right);
+                        camera_transform.translation.y = 0.0;
+                    } else {
+                        // level is taller than the screen
+                        orthographic_projection.right = (level.px_wid as f32 / 16.0).round() * 16.0;
+                        orthographic_projection.top = orthographic_projection.right / ASPECT_RATIO;
+                        camera_transform.translation.y = (player_translation.y
+                            - level_transform.translation.y
+                            - orthographic_projection.top / 2.0)
+                            .clamp(0.0, level.px_hei as f32 - orthographic_projection.top);
+                        camera_transform.translation.x = 0.0;
+                    }
+
+                    camera_transform.translation.x += level_transform.translation.x;
+                    camera_transform.translation.y += level_transform.translation.y;
+                }
+            }
+        }
+    }
 }
