49d6dd244e244fae1363764dc661ab4ab3d9b5bc
Hugo Bedard <hugo_bedard@hotmail.com>
2022-02-26 22:00:08 -0500

    Add system ordering

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use bevy_ecs_ldtk::prelude::*;
 mod components;
 mod systems;
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash, SystemLabel)]
+enum SystemOrder {
+    InputHandling,
+    WorldUpdate,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -18,13 +24,22 @@ fn main() {
             use_level_world_translations: true,
             ..Default::default()
         })
-        .add_startup_system(systems::setup)
-        .add_system(systems::camera_fit_inside_current_level)
-        .add_system(systems::generate_collision_map)
-        .add_system(systems::movement)
-        .add_system(systems::update_world)
         .register_ldtk_int_cell::<components::WallBundle>(1)
         .register_ldtk_int_cell::<components::LadderBundle>(2)
         .register_ldtk_entity::<components::PlayerBundle>("Player")
+        .add_startup_system(systems::setup)
+        .add_system(systems::generate_collision_map)
+        .add_system_set(
+            SystemSet::new()
+                .label(SystemOrder::InputHandling)
+                .with_system(systems::movement),
+        )
+        .add_system_set(
+            SystemSet::new()
+                .label(SystemOrder::WorldUpdate)
+                .after(SystemOrder::InputHandling)
+                .with_system(systems::camera_fit_inside_current_level)
+                .with_system(systems::update_world),
+        )
         .run();
 }
