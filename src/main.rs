03b5e33a226a8a1b438b6a074d12a9de28917ca3
Joel Robichaud <robichaud.jo@gmail.com>
2022-02-28 17:37:44 -0500

    Update bevy_ecs_ldtk and hook wasm errors to console.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ enum SystemOrder {
 }
 
 fn main() {
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(LdtkPlugin)
